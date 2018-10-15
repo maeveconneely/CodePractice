42b25c003c1068a66b117bfdf40d31dcdb0ce7f2
maeveconneely <42473084+maeveconneely@users.noreply.github.com>
2018-10-15 14:58:07 -0500

    Main class for substrings

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+class Main
+{
+  public static void main(String args[])
+  {
+    Parser parsing_boi = new Parser();
+    parsing_boi.calculateToInt();
+    parsing_boi.calculateToString();
+    parsing_boi.calculatePhoneNumber();
+  }
+}
